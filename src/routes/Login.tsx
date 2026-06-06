bcdb979ddcaede4bb95b3de773993538439ac914
jun-0411 <jun411@snu.ac.kr>
2026-06-06 16:12:09 +0900

    💄 Fix typo in password warning

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -45,7 +45,7 @@ export default function Login() {
                 type="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                placeholder="8자 이상, 숫자 포함"
+                placeholder="8자 이상, 영문, 숫자 포함"
                 required
               />
             </Field>
